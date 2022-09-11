c791357e0677088cc2ff03d40cec809103364550
= <76081373+tamaraafvieira@users.noreply.github.com>
2022-09-11 20:31:31 -0300

    testes, migrations e deploy

--- test/teste_contrato_grupo2.js
+++ test/teste_contrato_grupo2.js
@@ -1,13 +1,24 @@
-const TesteContratoGrupo2 = artifacts.require("TesteContratoGrupo2");
+const ContratoGrupo2 = artifacts.require("ContratoGrupo2");
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
-contract("TesteContratoGrupo2", function (/* accounts */) {
-  it("should assert true", async function () {
-    await TesteContratoGrupo2.deployed();
-    return assert.isTrue(true);
+
+contract("ContratoGrupo2", (accounts) => {
+  let instance;
+
+  beforeEach('Deve fazer o setup da instancia do contrato', async () => {
+  instance = await ContratoGrupo2.deployed();
   });
+
+	// estou testando se o contrato foi deployado
+	it("Deve voltar verdadeiro se foi deployado", async () => {
+		await ContratoGrupo2.deployed();
+		return assert.isTrue(true);
+	});
+
+
 });
+
+
+
+
+
+
