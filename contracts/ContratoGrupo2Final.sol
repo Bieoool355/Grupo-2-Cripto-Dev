fe9be99451ce716631d2d54210a1b209a83a5cc5
= <76081373+tamaraafvieira@users.noreply.github.com>
2022-09-12 16:33:42 -0300

    final

--- contracts/ContratoGrupo2Final.sol
+++ contracts/ContratoGrupo2Final.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract ContratoGrupo2 {
+contract ContratoGrupo2Final {
   address payable public owner;
   string public appName;
   uint public amountMaterial = 0;
