077eab16e7377597bdf0e630944530875d596a63
Bieoool355 <bielmengao_@hotmail.com>
2022-09-11 23:28:21 -0300

    Limpando comentários desnecessários

--- contracts/ContratoGRupo2.sol
+++ contracts/ContratoGRupo2.sol
@@ -62,8 +62,6 @@ contract ContratoGrupo2 {
     amountMaterial ++;
 
     // Criar um produto (add ao estoque)
-    // Pedir pra prof explicar essa linha, o mapping e o msg.sender / [dentro de colchetes é o índice, materials é o array]
-    // msg.sender quem está se conectando e usando o contrato no momento, como se fosse um this no JS.
     materials[amountMaterial] = Material(amountMaterial, _nameMaterial, _descriptionMaterial, _priceMaterial, payable(msg.sender), false);
 
     // Chamar o evento de criar o produto - o emit chama o evento
@@ -73,12 +71,11 @@ contract ContratoGrupo2 {
   // Função para a compra de material
   function buyMaterial(uint _idMaterial) public payable {
     // Armazenar os ids dos produtos em um array
-    Material memory _material = materials[_idMaterial]; // [exemplo pratico do indice sendo usado para buscar o id do produto]
+    Material memory _material = materials[_idMaterial]; 
 
     // Setar o dono do produto, o vendedor será o dono do produto
     address payable _seller = _material.owner;
 
-    
     // Verificação: se o produto possui um id válido   
     require(_material.idMaterial > 0 && _material.idMaterial <= amountMaterial, unicode"ERRO: id do material inválido.");
     
@@ -97,19 +94,17 @@ contract ContratoGrupo2 {
     // Setar que o produto foi vendido
     _material.sold = true;
 
-    //Dúvida: não entendemos como isso aqui funciona R: o produto na venda está igualando ao produto armazenado no estoque, direcionando para tira-lo do armazenamento
-    // Atualizar o estoque (depois ver se iremos colocar em outra função)
+    // Atualizar o estoque
     materials[_idMaterial] = _material;
 
     //Enviar o pagamento para o vendedor. O vendedor recebe a transferência do comprador
-    //A funcao transfer é pra transferir
     payable(_seller).transfer(msg.value);
 
-    //Evento do produto vendido (colocamos como true, pois inicialmente ele era false)
-    //Dúvida: porquê precisa do evento??? O que ele faz? Pq os nomes dos parametros sao os do struct? R: o evento de venda pega os atributos do produto em si.
+    //Evento do produto vendido
     emit SoldMaterial(amountMaterial, _material.nameMaterial, _material.descriptionMaterial, _material.priceMaterial, payable(msg.sender), true);
     
   }
+    //Função para atualizar o valor do produto
     function update(uint _idMaterial, string memory _nameMaterial, string memory _descriptionMaterial, uint _priceMaterial) public {
 
       materials[amountMaterial] = Material(_idMaterial, _nameMaterial, _descriptionMaterial, _priceMaterial, payable(msg.sender),false);
@@ -118,20 +113,15 @@ contract ContratoGrupo2 {
 
     }
 
-
   //  function withdraw(uint _amount) external {
   //       require(address(this).balance >= _amount, unicode"ERRO: Não há ether o suficiente na máquina!");
   //       owner.transfer(_amount * 1 ether);
   //  }
+
+   //Função para que o owner possa sacar o dinheiro da máquina
    function withdraw(uint _amount) external {        
         require(address(this).balance >= _amount, unicode"ERRO: Não há ether o suficiente na máquina!");
         payable(owner).transfer(_amount);
    }
-
-
-
-
-
-
 
 }
